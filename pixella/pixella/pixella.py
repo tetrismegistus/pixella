0930a4c71ecdf869c3d6fdffd5e07df8e74886e1
Aric Maddux <madducks@gmail.com>
2019-03-30 19:33:41 -0400

    added test and function for get_pixels

--- pixella/pixella/pixella.py
+++ pixella/pixella/pixella.py
@@ -5,3 +5,8 @@ from PIL import Image
 class Downsampler:
     def downsample(self, filename):
         return Image.open('out.png')
+
+    def get_pixels(self, filename):
+        img = Image.open(filename)
+        return list(img.getdata())
+
--- pixella/tests/test_pixella.py
+++ pixella/tests/test_pixella.py
@@ -21,3 +21,8 @@ def test_downsample():
     from PIL import Image
     assert result == Image.open('out.png')
 
+def test_getpixels_getimages():
+    downsampler = pixella.Downsampler()
+    result = downsampler.get_pixels('wbrgb.png')
+    desired_values = [(255, 255, 255), (0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    assert result == desired_values
