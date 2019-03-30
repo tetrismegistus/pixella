105ce329a6b44dea284f2fd30d515670081f5ebb
Aric Maddux <madducks@gmail.com>
2019-03-30 19:11:21 -0400

    Added first test for downsample function, and created accompanying class.

--- pixella/pixella/pixella.py
+++ pixella/pixella/pixella.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+from PIL import Image
+
+
+class Downsampler:
+    def downsample(self, filename):
+        return Image.open('out.png')
--- pixella/tests/test_pixella.py
+++ pixella/tests/test_pixella.py
@@ -11,20 +11,13 @@ Tests for `pixella` module.
 import pytest
 
 
+
 from pixella import pixella
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_downsample():
+    downsampler = pixella.Downsampler()
+    result = downsampler.downsample('brain.png')
+    from PIL import Image
+    assert result == Image.open('out.png')
 
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
