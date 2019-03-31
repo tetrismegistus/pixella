ab3b488db5f0a4ff7236ea418f2755d90dd26d15
Aric Maddux <madducks@gmail.com>
2019-03-30 22:15:20 -0400

    Added 2 tests and refactored

--- pixella/pixella/pixella.py
+++ pixella/pixella/pixella.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
+from PIL import ImageDraw
 
 
 class Downsampler:
-    def __init__(self, filename):
+    def __init__(self, filename, rescale=False, sample_rate=10):
         self.filename = filename
-
-    def downsample(self):
-        return Image.open('out.png')
-
-    def get_pixels_as_rows(self):
         img = Image.open(self.filename)
-        w, h = img.size
+        self.w, self.h = img.size
+        self.pixels_as_rows = self.get_pixels_as_rows(img)
+        new_image = self.get_new_image(rescale=rescale, sample_rate=sample_rate)
+
+    def downsample(self, img, sample_rate=10):
+        drawing = ImageDraw.Draw(img)
+        for row in range(0, self.h, sample_rate):
+            for col in range(0, self.w, sample_rate):
+                fill = self.pixels_as_rows[row][col]
+                drawing.rectangle([(col, row), (col + sample_rate, row + sample_rate)], fill=fill)
+        return img
+
+    def get_pixels_as_rows(self, img):
         pixel_list = list(img.getdata())
-        return [pixel_list[i * w:(i + 1) * w] for i in range(h)]
+        return [pixel_list[i * self.w:(i + 1) * self.w] for i in range(self.h)]
+
+    def get_new_image(self, rescale=False, sample_rate=None):
+        if rescale:
+            def rescale(x): return int(x / sample_rate) + 1
+            return Image.new('RGB', size=(rescale(self.w), rescale(self.h)))
+
+        return Image.new('RGB', size=(self.w, self.h))
--- pixella/tests/test_pixella.py
+++ pixella/tests/test_pixella.py
@@ -7,24 +7,47 @@ test_pixella
 
 Tests for `pixella` module.
 """
-
-import pytest
-
-
-
 from pixella import pixella
 
 
 def test_downsample_brain():
     downsampler = pixella.Downsampler('brain.png')
-    result = downsampler.downsample()
     from PIL import Image
-    assert result == Image.open('out.png')
+    img = downsampler.get_new_image()
+    result = downsampler.downsample(img)
+    comparison = Image.open('out.png')
+    assert list(comparison.getdata()) == list(result.getdata())
 
 
 def test_get_pixels_as_rows():
-    downsampler = pixella.Downsampler('wbrgb.png')
-    result = downsampler.get_pixels_as_rows()
+    file = 'wbrgb.png'
+    downsampler = pixella.Downsampler(file)
+    from PIL import Image
+    img = Image.open(file)
+    result = downsampler.get_pixels_as_rows(img)
     desired_values = [[(255, 255, 255), (0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255)],
                       [(0, 0, 255), (0, 0, 255), (0, 0, 255), (0, 0, 255), (0, 0, 255)]]
     assert result == desired_values
+
+
+def test_get_new_image_surface_no_size_loss():
+    downsampler = pixella.Downsampler('100x100.png')
+    result = downsampler.get_new_image()
+    w, h = result.size
+    assert w == 100
+    assert h == 100
+
+def test_get_new_image_no_size_loss():
+    downsampler = pixella.Downsampler('100x100.png')
+    result = downsampler.get_new_image()
+    w, h = result.size
+    assert w == 100
+    assert h == 100
+
+def test_get_new_image_shrink():
+    downsampler = pixella.Downsampler('100x100.png')
+    result = downsampler.get_new_image(rescale=True, sample_rate=20)
+    w, h = result.size
+    assert w == 6
+    assert h == 6
+
